4d7f6eec8d0cfb8735f16f541266f353ca8b2053
Martin Formanek <martin.formanek88@gmail.com>
2019-08-28 18:19:25 -0700

    My homework

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are creating a special data structure
+## which can store matrix and its inverse and recall
+## it without repeating the computation
 
-## Write a short comment describing this function
+## z<-makeCacheMatrix(mymatrix) creates an object containing a 
+## matrix which can be accessed using z$get() and its
+## inverse set by z$setinverse(inv) and recalled by
+## z$getinverse(inv)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of the matrix
+## passed by the object defined above and stores its
+## value to its inv attribute. If this inv already exists
+## it doesn't calculate again and just recalls the value.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)){
+        message("getting cached inverse matrix")
+        return(inv)
+    }
+    matrix <- x$get()
+    inv <- solve(matrix,...)
+    x$setinverse(inv)
+    inv
 }
